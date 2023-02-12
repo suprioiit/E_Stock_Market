f945cf9f0765a4828bb9a9162dc67bf94b611709
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2023-02-12 13:21:33 +0530

    first commit by suprio

---
+++ src/main/java/com/stockmarket/suprio/SuprioApplication.java
@@ -0,0 +1,13 @@
+package com.stockmarket.suprio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuprioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuprioApplication.class, args);
+	}
+
+}
